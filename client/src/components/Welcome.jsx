197869a499e18781e3ca84f2c9749c1f4ab7ac1b
dmitridrozdov <dm.drozdov@gmail.com>
2022-06-30 14:18:51 +1000

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -21,6 +21,22 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
+  // const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext(TransactionContext);
+
+  // const handleSubmit = (e) => {
+  //   const { addressTo, amount, keyword, message } = formData;
+
+  //   e.preventDefault();
+
+  //   if (!addressTo || !amount || !keyword || !message) return;
+
+  //   sendTransaction();
+  // };
+
+  const connectWallet = () => {
+    
+  }
+
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -28,6 +44,21 @@ const Welcome = () => {
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
+          <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+            Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+          </p>
+          {/* {!currentAccount && ( */}
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            >
+              {/* <AiFillPlayCircle className="text-white mr-2" /> */}
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
+              </p>
+            </button>
+          {/* )} */}
         </div>
       </div>
     </div>
