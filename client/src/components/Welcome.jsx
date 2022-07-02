a50b45bb6d8ca999515b57e4c57a1663f889d024
dmitridrozdov <dm.drozdov@gmail.com>
2022-07-02 10:33:39 +1000

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -117,7 +117,7 @@ const Welcome = () => {
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
             {/* {isLoading */}
-            {false
+            {true
               ? <Loader />
               : (
                 <button
